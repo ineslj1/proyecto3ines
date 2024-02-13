0df62badd95901197d7aed48109ef084f288ee44
ineslj1 <ineslopezjuan@gmail.com>
2024-02-13 13:17:03 +0100

    lastchanges

--- src/components/galleryCard/galleryCard.js
+++ src/components/galleryCard/galleryCard.js
@@ -2,20 +2,16 @@ import './galleryCard.css';
 
 export const createCard = (
   parentElement,
-  { urls: imgSrc, description, alt_description, user }
+  { urls: imgSrc, description, alt_description}
 ) => {
   const card = document.createElement('article');
   card.classList.add('card');
   card.innerHTML = `
 <div class="img-container">
-  <img src="${imgSrc.small}" alt="${alt_description}" loading="lazy" />
+  <img src="${imgSrc.small}" alt="${alt_description}" />
 </div>
 <div class="description">
   <h3>${description ?? ''}</h3>
-  <div class="user flex-container">
-    <div class="img-container"><img src=${user.profile_image.medium}/></div>
-    <h4>${user.name}</h4>
-  </div>
 </div>
 `;
   parentElement.append(card);
